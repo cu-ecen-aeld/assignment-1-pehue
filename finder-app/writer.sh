a7ed94ed8889c3b75965dd8a9c8a3b4ec8cbb2b1
pehue <peter.huesser@psi.ch>
2022-07-20 18:45:23 +0200

    - added solutions

---
+++ finder-app/writer.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+if [[ $# != 2 ]]
+then
+	echo "Something is wrong with the parameters"
+	exit 1 
+fi
+
+writefile=$1
+writestr=$2
+
+dname=`dirname ${writefile}`
+fname=`basename ${writefile}`
+
+if ! [[ -d ${filesdir} ]]
+then
+	mkdir -p ${dname}
+fi
+
+echo ${writestr} > ${writefile}
+
+exit 0
+
