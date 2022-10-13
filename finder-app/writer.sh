60f7b42864748fd5bc5d2a49d27c2e3baa0acde7
pehue <peter.huesser@psi.ch>
2022-10-13 12:46:23 +0200

    bash scripts

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,23 +1,20 @@
 #!/usr/bin/bash
 
-if [[ $# != 2 ]]
-then
-	echo "Something is wrong with the parameters"
-	exit 1 
+if [[ $# != 2 ]]; then
+  echo "Something is wrong with the parameters"
+  exit 1
 fi
 
 writefile=$1
 writestr=$2
 
-dname=`dirname ${writefile}`
-fname=`basename ${writefile}`
+dname=$(dirname ${writefile})
+fname=$(basename ${writefile})
 
-if ! [[ -d ${filesdir} ]]
-then
-	mkdir -p ${dname}
+if ! [[ -d ${filesdir} ]]; then
+  mkdir -p ${dname}
 fi
 
-echo ${writestr} > ${writefile}
+echo ${writestr} >${writefile}
 
 exit 0
-
